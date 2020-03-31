5d9f70e1fac176b109dd31c09cab2897c9a332bc
sakronos <yangcheng1999@foxmail.com>
2020-03-31 13:59:08 +0800

    3/31 Homework

--- app/src/main/java/com/example/RegisterDemo/MainActivity.java
+++ app/src/main/java/com/example/RegisterDemo/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.RegisterDemo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
@@ -18,42 +25,42 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private MyService.DownloadBinder downloadBinder;
-
-    private ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            downloadBinder = (MyService.DownloadBinder) service;
-            downloadBinder.startDownload();
-            downloadBinder.getProgress();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
-        }
-    };
+    private String channelId = "1";
+    private String channelName = "Button";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bind_service).setOnClickListener(this);
-        findViewById(R.id.unbind_service).setOnClickListener(this);
+        findViewById(R.id.send_notice).setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bind_service:
-                Intent bindIntent = new Intent(this, MyService.class);
-                bindService(bindIntent,connection,BIND_AUTO_CREATE);
-                Toast.makeText(this,"bindService",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.unbind_service:
-                unbindService(connection);
-                Toast.makeText(this,"unbindService",Toast.LENGTH_SHORT).show();
+            case R.id.send_notice:
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+                NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                @SuppressLint("WrongConstant")
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH);
+                manager.createNotificationChannel(notificationChannel);
+                Notification notification = new NotificationCompat.Builder(this,channelId)
+                        .setContentTitle("TITLE")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.penguin))
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .build();
+                assert manager != null;
+                manager.notify(1,notification);
+                Toast.makeText(this,"Notice",Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
