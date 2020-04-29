9d38a2d8164ced810db70d78d4849df30f36e251
sakronos <yangcheng1999@foxmail.com>
2020-04-29 11:19:25 +0800

    4/29 上机报告1

--- app/src/main/java/com/example/RegisterDemo/MainActivity.java
+++ app/src/main/java/com/example/RegisterDemo/MainActivity.java
@@ -21,56 +21,68 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageView imageView;
+public class MainActivity extends AppCompatActivity {
+    private List<Fruit> fruitList = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.send_notice).setOnClickListener(this);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
-    private class UpdateImageView extends AsyncTask<Integer, Integer, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(Integer... integers) {
-            publishProgress(0);
-            return true;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            imageView.setImageDrawable(getResources().getDrawable(R.drawable.penguin,null));
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            //imageView.clearColorFilter();
-            //imageView.setImageDrawable(getResources().getDrawable(R.drawable.penguin,null));
-            super.onPostExecute(aBoolean);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.send_notice:
-                new UpdateImageView().execute();
-                Toast.makeText(this,"点击",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
+    private void initFruits(){
+        for (int i = 0; i<2; i++){
+            Fruit apple = new Fruit ("Apple", R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit avocado = new Fruit("Avocado",R.drawable.avocado_pic);
+            fruitList.add(avocado);
+            Fruit banana = new Fruit ("Banana", R.drawable.banana_pic);
+            fruitList.add( banana);
+            Fruit orange = new Fruit ( "Orange", R.drawable.orange_pic) ;
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit ( "Watermelon", R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            fruitList.add(pear);
+            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit("Pineapple" , R.drawable.pineapple_pic);
+            fruitList.add( pineapple);
+            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit ("Cherry", R.drawable.cherry_pic);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit("Mango",R.drawable.mango_pic);
+            fruitList.add(mango) ;
         }
     }
+
+
+
 }
