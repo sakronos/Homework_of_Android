0655084b7737299de27360c0d0a0c67ab5316a5d
sakronos <yangcheng1999@foxmail.com>
2020-05-26 18:52:19 +0800

    5/26 3RD上机 LitePal使用

---
+++ app/src/main/java/com/example/RegisterDemo/Category.java
@@ -0,0 +1,34 @@
+package com.example.RegisterDemo;
+
+public class Category {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    private String categoryName;
+
+    private int categoryCode;
+}
--- app/src/main/java/com/example/RegisterDemo/MainActivity.java
+++ app/src/main/java/com/example/RegisterDemo/MainActivity.java
@@ -2,95 +2,114 @@ package com.example.RegisterDemo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.IBinder;
+
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.util.ArrayList;
+
+import org.litepal.LitePal;
+
+import org.litepal.tablemanager.Connector;
+
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private List<Msg> msgList = new ArrayList<>();
-    private EditText inputText;
-    private Button send;
-    private RecyclerView msgRecyclerView;
-    private MsgAdapter adapter;
 
-    private void initMsgs(){
-        Msg msg1 = new Msg("Hello guy.",Msg.TYPE_RECEIVER);
-        msgList.add(msg1);
-        Msg msg2 = new Msg("Hello. Who is that?",Msg.TYPE_SENT);
-        msgList.add(msg2);
-        Msg msg3 = new Msg("This is Tom Nook. Nice talking to you.",Msg.TYPE_RECEIVER);
-        msgList.add(msg3);
-        Msg msg4 = new Msg(R.drawable.apple_pic,Msg.TYPE_IMG_SENT);
-        msgList.add(msg4);
-        Msg msg5 = new Msg(R.drawable.cherry_pic,Msg.TYPE_IMG_RECEIVED);
-        msgList.add(msg5);
-    }
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initMsgs();
-        inputText = (EditText) findViewById(R.id.input_text);
-        send = (Button) findViewById(R.id.send);
-        msgRecyclerView = (RecyclerView)findViewById(R.id.msg_recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        msgRecyclerView.setLayoutManager(layoutManager);
-        adapter = new MsgAdapter(msgList);
-        msgRecyclerView.setAdapter(adapter);
-        send.setOnClickListener(new View.OnClickListener(){
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connector.getDatabase();
+                Toast.makeText(getApplicationContext(),"Database Successfully Created",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button addData = findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                String content = inputText.getText().toString();
-                if (!"".equals(content)){
-                    Msg msg = new Msg(content,Msg.TYPE_SENT);
-                    msgList.add(msg);
-                    adapter.notifyItemInserted(msgList.size()-1);
-                    msgRecyclerView.scrollToPosition(msgList.size()-1);
-                    inputText.setText("");
-                }
+                Book book = new Book();
+                book.setName("线代");
+                book.setAuthor("同济");
+                book.setPages(200);
+                book.setPrice(4);
+                book.setPress("高等教育");
+                book.save();
             }
         });
 
 
+        Button updateData = findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                //book.setName("The Lost Symbol");
+                //book.setAuthor("Dan Brown");
 
+                book.setPrice(19.95);
+                book.setPress("Anchor");
+                book.updateAll("name=? and author=?","The Da Vinci Code","Dan Brown");
 
+            }
+        });
+
+        Button deleteALLData = findViewById(R.id.delete_data);
+        deleteALLData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.deleteAll(Book.class,"price >?","15");
+            }
+        });
+
+        Button queryButton = findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.QueryResult);
+                StringBuilder text = new StringBuilder();
+                List<Book> books = LitePal.findAll(Book.class);
+                for (Book book:books){
+                    text.append("Name:").append(book.getName()).append("\n").append("Author:").append(book.getAuthor()).append("\n").append("Pages:").append(book.getPages()).append("\n").append("Price:").append(book.getPrice()).append("\n").append("Press:").append(book.getPress()).append("\n\n");
+                }
+                textView.setText(text.toString());
+            }
+        });
+
+        Button fluentQuery = findViewById(R.id.fluent_query);
+        fluentQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.QueryResult);
+                StringBuilder text = new StringBuilder();
+                List<Book> books = LitePal.select("name","author","pages")
+                        .where("pages >?","400")
+                        .order("pages")
+                        .limit(10)
+                        //.offset(10)       //偏移量
+                        .find(Book.class);
+                for (Book book:books){
+                    text.append("Name:").append(book.getName())
+                            .append("\n").append("Author:").append(book.getAuthor())
+                            .append("\n").append("Pages:").append(book.getPages()).append("\n\n");
+                }
+                textView.setText(text.toString());
+            }
+        });
 
     }
-
-
-
 
 
 }
